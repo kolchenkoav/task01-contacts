09e73eea90f3cbd54ee65dca6019535cdf6aee79
Aleksey <kolchenkoav@gmail.com>
2024-04-18 08:57:51 +0300

    SLF4J

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,16 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.config.AppConfig;
 import org.example.service.MyCommandHandler;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        log.info("Spring context initialized");
         context.getBean(MyCommandHandler.class).run();
     }
 }
